786ddf7a343850df48c86d1067a294a8dc0cdbfb
ibrahim halil sakli <63147096+softwareVirus@users.noreply.github.com>
2021-05-22 19:37:01 +0300

    Update isPrimeNumber.js
    It is unnecessary to check 3 different situations. One condition is enough.

--- CodeWars/isPrimeNumber.js
+++ CodeWars/isPrimeNumber.js
@@ -2,10 +2,10 @@ function isPrime(num) {
 
   let result;
 
-  if (num === 0 || num === 1 || num < 0) {
+  if (num < 2) {
     return false;
   }
-
+  
   for (let i = 2, j = Math.sqrt(num); i <= j; ++i) {
     if (num % i === 0) {
       return false;
